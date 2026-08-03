13b110383ae8e36678a025c28094a045d6aecf4d
Nevid-786 <mdnabid786@gmail.com>
2026-08-03 14:00:48 +0530

    completed Local installaion by Docker

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getCurrentUser, getLogout, postLogin, postsignup } from "../controller/authController.js";
 import VerifyJWT  from "../middleWare/verifyJWTmiddleware.js";
-import { apiLimiter, authLimiter, currentUserLimiter } from "../middleWare/rateLimiter.js";
+
 const authRouter=Router();
-authRouter.post("/login",authLimiter,postLogin)
-authRouter.post("/signup",authLimiter,postsignup)
-authRouter.get("/getuser",currentUserLimiter,VerifyJWT,getCurrentUser)
+authRouter.post("/login",postLogin)
+authRouter.post("/signup",postsignup)
+authRouter.get("/getuser",VerifyJWT,getCurrentUser)
 authRouter.get("/logout",VerifyJWT,getLogout)
 export default authRouter;
